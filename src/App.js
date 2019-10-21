c580053fb96d3cdaff077cc44d30a0bdf411fe6d
Ronald Connal <ron.connal@gmail.com>
2019-10-21 12:10:06 -0700

    Initiating changes ..
    Learning MUI elements and props ...

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
-import ProTip from './ProTip';
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Link from "@material-ui/core/Link";
+import ProTip from "./ProTip";
+import NavBar from "./components/NavBar";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
 export default function App() {
   return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
+    <Container maxWidth={false}>
+      <NavBar />
+      <Box my={6}>
+        <Typography variant="h3" component="h1" gutterBottom align="center">
           Create React App v4-beta example
         </Typography>
         <ProTip />
--- src/ProTip.js
+++ src/ProTip.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Link from '@material-ui/core/Link';
-import SvgIcon from '@material-ui/core/SvgIcon';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Link from "@material-ui/core/Link";
+import SvgIcon from "@material-ui/core/SvgIcon";
+import Typography from "@material-ui/core/Typography";
 
 function LightBulbIcon(props) {
   return (
@@ -14,22 +14,24 @@ function LightBulbIcon(props) {
 
 const useStyles = makeStyles(theme => ({
   root: {
-    margin: theme.spacing(6, 0, 3),
+    margin: theme.spacing(6, 0, 6)
   },
   lightBulb: {
-    verticalAlign: 'middle',
-    marginRight: theme.spacing(1),
-  },
+    verticalAlign: "middle",
+    marginRight: theme.spacing(1)
+  }
 }));
 
 export default function ProTip() {
   const classes = useStyles();
   return (
-    <Typography className={classes.root} color="textSecondary">
+    <Typography className={classes.root} color="textSecondary" align="center">
       <LightBulbIcon className={classes.lightBulb} />
-      Pro tip: See more{' '}
-      <Link href="https://material-ui.com/getting-started/templates/">templates</Link> on the
-      Material-UI documentation.
+      Pro tip: See more{" "}
+      <Link href="https://material-ui.com/getting-started/templates/">
+        templates
+      </Link>{" "}
+      on the Material-UI documentation.
     </Typography>
   );
 }
